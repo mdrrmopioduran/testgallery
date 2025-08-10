cc3309a5abbb1d1e949f40dd46a2fd7bebed38b9
mdrrmo pio duran <mdrrmopioduran@proton.me>
2025-08-11 00:54:59 +0800

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,8 @@
-//import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-//const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-//const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://mbcdnzkmqceipyhoxpfn.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 if (!supabaseUrl || !supabaseAnonKey) {
